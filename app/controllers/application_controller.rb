1afcb5ab7b1925e0f0cd95aa730402ff7b3ddfa2
naysayer <joshuagregoryshuster@gmail.com>
2012-11-09 19:13:22 -0800

    added table of incomplete tasks due within a month's time to the home page view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,11 @@ class ApplicationController < ActionController::Base
     %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
   end
   
+  def task_summary
+    task_month_strftime = DATE_MONTH_STRFTIME.gsub(/COLUMN/, "due_date") 
+    task_year_strftime = DATE_YEAR_STRFTIME.gsub(/COLUMN/, "due_date")
+    @tasks_this_month = Task.where("#{task_month_strftime} = ? AND #{task_year_strftime} = ? AND completed = ?", Time.now.month.to_s, Time.now.year.to_s, false)
+  end
+  
+  
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  before_filter :task_summary, :only => [:home]
   def home
     @task = Task.where(:completed => false).limit(10)
   end
