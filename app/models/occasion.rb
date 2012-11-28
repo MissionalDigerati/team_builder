d75f67ee95e0c7183662ec450cc9911b8f56b52d
naysayer <joshuagregoryshuster@gmail.com>
2012-11-27 17:17:52 -0800

    removed un-needed code from the occasion model.

--- app/models/occasion.rb
+++ app/models/occasion.rb
@@ -1,19 +1,4 @@
 class Occasion < ActiveRecord::Base
   belongs_to :contact
   attr_accessible :occasion, :contact_id, :special_date
-  
-  year = Time.new.year
-  
-  DAYS = Array(1..31)
-  MONTHS = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"]
-  YEARS = [year, year + 1, year + 2, year + 3]
-  
-  def self.special_date(day, month, year)
-    if year.present?
-      date = day + ' / ' + month + ' / ' + year
-    else
-      date = day + ' / ' + month
-    end
-  end
-  
 end
--- spec/models/occasion_spec.rb
+++ spec/models/occasion_spec.rb
@@ -1,7 +1,4 @@
 require 'spec_helper'
 
 describe Occasion do
-  context "methods" do
-    
-  end
 end
