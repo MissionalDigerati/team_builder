15c658b7ce8d032743fc27e1dcf2de1f1047bfe3
naysayer <joshuagregoryshuster@gmail.com>
2012-11-30 17:30:29 -0800

    Finished rspec test for validating last names presence.

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -10,6 +10,10 @@ describe Contact do
       FactoryGirl.build(:defaulted_contact, :first_name => nil).should_not be_valid
     end
     
+    it "should require last name" do
+      FactoryGirl.build(:defaulted_contact, :last_name => nil).should_not be_valid
+    end
+    
     it "should require email" do
       FactoryGirl.build(:defaulted_contact, :email => nil).should_not be_valid
     end
