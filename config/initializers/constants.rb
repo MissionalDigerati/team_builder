d9b5afbdf5f15792d1619cf746ffe2603a4dc291
Codemis <johnathan@missionaldigerati.com>
2014-06-17 10:36:58 -0700

    Added constant for sorting the special occassions.

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -3,10 +3,12 @@ if (db_adapter=='sqlite3')
   DATE_DAY_STRFTIME = "strftime('%d', COLUMN)"
   DATE_MONTH_STRFTIME = "strftime('%m', COLUMN)"
   DATE_YEAR_STRFTIME = "strftime('%Y', COLUMN)"
+  DATE_MONTH_DAY_ORDER_STRFTIME = "strftime('%d %m', COLUMN) DIRECTION"
 elsif db_adapter["mysql"]
   DATE_DAY_STRFTIME = "extract(day from COLUMN)"
   DATE_MONTH_STRFTIME = "extract(month from COLUMN)"
   DATE_YEAR_STRFTIME = "extract(year  from COLUMN)"
+  DATE_MONTH_DAY_ORDER_STRFTIME = "extract(month from COLUMN) DIRECTION, extract(day from COLUMN) DIRECTION"
 else
   puts "insert rails error"
 end
