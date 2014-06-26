0944920a1cb12a1414b67a347ec468a1b084090a
Codemis <johnathan@missionaldigerati.com>
2014-06-25 20:52:30 -0700

    Changed helper method widget_donation_data to widget_donation_this_year_data.

--- app/helpers/widget_helper.rb
+++ app/helpers/widget_helper.rb
@@ -1,6 +1,6 @@
 module WidgetHelper
 
-  def widget_donation_data
+  def widget_donation_this_year_data
     donations = Donation.monthly_sums(Time.now.strftime("%Y").to_s)
     donations.each_with_index.map { |x,i| [i, x] }
   end
