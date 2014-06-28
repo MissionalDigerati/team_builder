f26c691a01327b9b20a020eb95b85abe627398a2
Codemis <johnathan@missionaldigerati.com>
2014-06-28 15:12:16 -0700

    Moved calendar js into a seperate file for better organization.

---
+++ app/assets/javascripts/occasion_calendar.js
@@ -0,0 +1,36 @@
+/**
+ * Setup the calendar for the occasions.  Run this method once the document is loaded.
+ *
+ * @param String eventsPath the path to events JSON data
+ * @return void
+ */
+function setUpCalendar(eventsPath) {
+    $('#special-dates-calendar').fullCalendar({
+        'header': {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        },
+        'events': eventsPath,
+        eventBackgroundColor: '#5eb2d9',
+        textColor: '#FFFFFF',
+        color: '#5eb2d9',
+        eventAfterRender: callbackEventsAfterRender
+    });
+};
+/**
+ * The callback after the event is rendered on the screen
+ *
+ * @param Object event the event JSON object
+ * @param Object element the element JQuery object that contains the event
+ * @return void
+ */
+function callbackEventsAfterRender(event, element) {
+    element.popover({
+                title: event.title,
+                trigger: 'hover',
+                placement: 'top',
+                content: "Event for "+event.contact.name,
+                container: 'body'
+            });
+};
