9c81d700673befe2ed729799c5e4beaa2379a939
Codemis <johnathan@missionaldigerati.com>
2014-06-27 17:34:07 -0700

    Added the fullcalendar js.

--- app/models/occasion.rb
+++ app/models/occasion.rb
@@ -1,4 +1,9 @@
 class Occasion < ActiveRecord::Base
-  belongs_to :contact
-  attr_accessible :occasion, :contact_id, :special_date
+    belongs_to :contact
+    attr_accessible :occasion, :contact_id, :special_date
+    acts_as_birthday :special_date
+
+    def calendar_date
+        Date.parse("#{Date.today.year}-#{self.special_date.month}-#{self.special_date.day}")
+    end
 end
--- spec/models/occasion_spec.rb
+++ spec/models/occasion_spec.rb
@@ -1,9 +1,20 @@
 require 'spec_helper'
 
 describe Occasion do
-  context "validation" do
-    it "should created a valid user" do
-      FactoryGirl.create(:defaulted_occasion).should be_valid
+    context "validation" do
+        it "should created a valid user" do
+            FactoryGirl.create(:defaulted_occasion).should be_valid
+        end
     end
-  end
+
+    context "methods" do
+        context "#calendar_date" do
+
+            it "should return a date with the current year if it is reoccuring" do
+                expected_year = Date.today.year
+                occasion = FactoryGirl.create(:defaulted_occasion, { special_date: Date.parse('2001-12-01') })
+                occasion.calendar_date.year.should == expected_year
+            end
+        end
+    end
 end
