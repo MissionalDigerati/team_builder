7d8cb63e31ae12d9519b3c2debe95f47bdd996cf
Codemis <johnathan@missionaldigerati.com>
2014-07-02 14:28:03 -0700

    Ran rake task for upgrading Rails.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
-TeamBuilder::Application.routes.draw do
+Rails.application.routes.draw do
+
   resources :contacts do
     resources :support_states do
       member do
@@ -17,63 +18,11 @@ TeamBuilder::Application.routes.draw do
       get :download
     end
   end
-  match 'reports/progress' => 'reports#progress', method: :get
-  match 'tasks/completed/:id' => 'tasks#completed', method: :get, as: :complete_task
+
+  get 'reports/progress', to: 'reports#progress'
+  get 'tasks/completed/:id', to: 'tasks#completed', as: :complete_task
   get 'tags/:tag', to: 'contacts#index', as: :tag
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
+  root 'pages#home'
 
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  root :to => 'pages#home'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
