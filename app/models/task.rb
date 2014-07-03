699a826288fa1e8d9887050ee6d49315845291d1
Codemis <johnathan@missionaldigerati.com>
2014-07-03 16:32:39 -0700

    Added strong parameters to tasks.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,6 @@
 class Task < ActiveRecord::Base
 	
     belongs_to :contact
-    attr_accessible :contact_id, :completed, :task, :due_date, :category
     validates :task, :due_date, :category, :contact_id, :presence => true
 
     CATEGORY = ["Call", "Email", "Follow Up", "Lunch", "Meeting", "Money Presentation", "Trip", "Other"]
