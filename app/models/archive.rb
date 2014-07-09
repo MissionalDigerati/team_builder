6fb79d796e8806f57ed844a7d60195b834ff02de
Codemis <johnathan@missionaldigerati.com>
2014-07-08 17:33:37 -0700

    Remove attr_accesible from archive.

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -1,7 +1,6 @@
 class Archive < ActiveRecord::Base
-  attr_accessible :filename
-  
-  #the archive controller calls this method to generate the filename
+  # the archive controller calls this method to generate the filename
+  #
   def self.file_name
     Time.now.to_i
   end
