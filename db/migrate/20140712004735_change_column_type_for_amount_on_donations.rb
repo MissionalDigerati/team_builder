3892fed68f85b087e5a064490557396c4a087511
Codemis <johnathan@missionaldigerati.com>
2014-07-16 15:36:23 -0700

    Fixed the revert down to work correctly.

--- db/migrate/20140712004735_change_column_type_for_amount_on_donations.rb
+++ db/migrate/20140712004735_change_column_type_for_amount_on_donations.rb
@@ -16,10 +16,25 @@ class ChangeColumnTypeForAmountOnDonations < ActiveRecord::Migration
     # remove the older status column
     remove_column :donations, :amount
     # rename the convert_status to status column
-    rename_column :donations,:convert_amount,:amount
+    rename_column :donations, :convert_amount, :amount
   end
 
   def self.down
-    change_column :donations, :amount, :text, 'text USING CAST(amount AS text)'
+    add_column :donations, :convert_amount, :text
+    
+    # look up the schema's to be able to re-inspect the Donation model
+    # http://apidock.com/rails/ActiveRecord/Base/reset_column_information/class
+    Donation.reset_column_information
+    
+    # loop over the collection
+    Donation.all.each do |d|
+        d.convert_amount = d.amount.to_s
+        d.save
+    end
+    
+    # remove the older status column
+    remove_column :donations, :amount
+    # rename the convert_status to status column
+    rename_column :donations, :convert_amount, :amount
   end
 end
