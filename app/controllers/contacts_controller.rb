639b9c43967a9b7ceabd2174fccc8e0acf616485
Codemis <johnathan@missionaldigerati.com>
2014-07-18 16:38:15 -0700

    Added team status to contact form.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -110,7 +110,7 @@ class ContactsController < ApplicationController
       params.require(:contact).permit(
         :first_name, :last_name, :spouse_name, :email, :spouse_email, :network, :address_1, :address_2, :city, :province,
         :state_id, :zip, :country_id, :receive_newsletter, :children, :preferred_contact, :believer, :spouse_believer, 
-        :presented_vision, :avatar, :account_number, :tag_list, 
+        :presented_vision, :avatar, :account_number, :tag_list, :team_status, 
         support_state_attributes: 
           [
             :contact_id, :initial, :letter_sent, :letter_sent_on, :contacting, :seen_presentation, :presented_on, 
