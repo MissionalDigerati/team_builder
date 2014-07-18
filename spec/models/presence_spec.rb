71ea64f24ca822e549c8dfa5bc1bb47d097182d8
Codemis <johnathan@missionaldigerati.com>
2014-07-18 12:32:09 -0700

    Removed the need to add the http on the URL.

--- spec/models/presence_spec.rb
+++ spec/models/presence_spec.rb
@@ -6,4 +6,19 @@ describe Presence, :type => :model do
       expect(FactoryGirl.build(:defaulted_presence)).to be_valid
     end
   end
+  context "callbacks" do
+
+    it "should not append an http if the url has an http" do
+      expected_url = 'http://www.twitter.com'
+      occasion = FactoryGirl.create(:defaulted_presence, { url: 'http://www.twitter.com' })
+      expect(occasion.url).to eq(expected_url)
+    end
+
+    it "should append an http if it is missing on a url" do
+      expected_url = 'http://www.twitter.com'
+      occasion = FactoryGirl.create(:defaulted_presence, { url: 'www.twitter.com' })
+      expect(occasion.url).to eq(expected_url)
+    end
+
+  end
 end
