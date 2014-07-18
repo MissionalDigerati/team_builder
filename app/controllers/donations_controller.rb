a4b56ed576be3fb0ae1c70653f21904f40368252
Codemis <johnathan@missionaldigerati.com>
2014-07-17 18:10:00 -0700

    Removed plural for the single resource.

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -12,50 +12,50 @@ class DonationsController < ApplicationController
   
   def new
     if params[:contact_id] == nil
-      @donations = Donation.new
+      @donation = Donation.new
     else
       @contact = Contact.find(params[:contact_id])
-      @donations = @contact.donations.build
+      @donation = @contact.donations.build
     end
   end
   
   def create
     @contact = Contact.find(params[:donation][:contact_id])
-    @donations = @contact.donations.new(donation_params)
+    @donation = @contact.donations.new(donation_params)
     respond_to do |format|
-      if @donations.save
+      if @donation.save
         format.html {redirect_to(contact_path(@contact))}
         flash[:notice] = "Your Donation has been saved!"
       else
         format.html {render action: "new"}
-        flash[:notice] = @donations.errors.empty? ? "Your Donation has not been saved" : "Your Donation has not been saved because: " + @donations.errors.full_messages.to_sentence
+        flash[:notice] = @donation.errors.empty? ? "Your Donation has not been saved" : "Your Donation has not been saved because: " + @donation.errors.full_messages.to_sentence
       end
     end
   end
   
   def edit
     @contact = Contact.find(params[:contact_id])
-    @donations = @contact.donations.find(params[:id])
-    @donations.amount = "%.2f" % @donations.amount
+    @donation = @contact.donations.find(params[:id])
+    @donation.amount = "%.2f" % @donation.amount
   end
   
   def update
-    @donations = Donation.find(params[:id])
-    @contact = Contact.find(@donations.contact_id)
+    @donation = Donation.find(params[:id])
+    @contact = Contact.find(@donation.contact_id)
     respond_to do |format|
-      if @donations.update_attributes(donation_params)
+      if @donation.update_attributes(donation_params)
         format.html {redirect_to(contact_path(@contact))}
         flash[:notice] = "Your Donation has been updated!"
       else
         format.html {render action: "edit"}
-        flash[:notice] = @donations.errors.empty? ? "Your Donation has not been updated" : "Your Donation has not been updated because: " + @donations.errors.full_messages.to_sentence
+        flash[:notice] = @donation.errors.empty? ? "Your Donation has not been updated" : "Your Donation has not been updated because: " + @donation.errors.full_messages.to_sentence
       end
     end
   end
   
   def destroy
-     @donations = Donation.find(params[:id])
-     @donations.destroy
+     @donation = Donation.find(params[:id])
+     @donation.destroy
      respond_to do |format|
        format.html { redirect_to :back }
        flash[:notice] = "Your Donation has been deleted."
